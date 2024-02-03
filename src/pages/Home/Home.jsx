218c848395b34a999747a4b450a951fd4683c208
marwa <alqtta04@gmail.com>
2024-02-03 18:43:12 +0200

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home-page'><header className='h-100 min-vh-100'>
+      </header></div>
+  )
+}
+
+export default Home
